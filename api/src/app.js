c703c7669a4d8632dbbd973b1eb89e0b352e89bb
Francisco Agustin García <franciscoagustingarciagarcia@gmail.com>
2025-11-17 18:26:39 -0300

    cambios

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,12 @@
 import express from "express";
-import cors from "cors";
+//import cors from "cors";
 import empresaRoutes from "./routes/empresa.js";
 import adminRoutes from "./routes/administrador.js";
 import conductorRoutes from "./routes/conductor.js";
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- api/src/config/dotenv.js
+++ api/src/config/dotenv.js
@@ -1,14 +1,14 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config({ path: './api/.env' });
+dotenv.config();
 
-console.log('Variables cargadas:', process.env.DB_HOST, process.env.DB_USER);
+console.log("Variables cargadas:", process.env.DB_HOST, process.env.DB_USER);
 
 export const {
-  DB_HOST = 'localhost',
-  DB_USER = 'root',
-  DB_PASS = 'root',
-  DB_NAME = 'cargolink',
+  DB_HOST = "localhost",
+  DB_USER = "root",
+  DB_PASS = "root",
+  DB_NAME = "cargolink",
   DB_PORT = 3306,
   PORT = 3000,
 } = process.env;
